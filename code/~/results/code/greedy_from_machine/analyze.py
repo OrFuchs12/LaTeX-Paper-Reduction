2b536bab495a1a53328b1de2b154279e5177c037
tala12397 <abrahamt@post.bgu.ac.il>
2024-02-11 17:05:05 +0200

    with plots to the data

---
+++ code/~/results/code/greedy_from_machine/analyze.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+current_directory = os.getcwd()
+
+csv_directory = "code/~/results/code/greedy_from_machine"
+
+file_paths = [
+    "files_non_stop_results_classification_greedy.csv",
+    "files_results_heuristic_greedy.csv",
+    "files_results_model_greedy.csv",
+    "files_results_non_stop_heuristic_greedy.csv",
+    "files_results_non_stop_regreession_model_greedy.csv",
+    "files_results_regreession_model_greedy.csv",
+    "files_results_simple_greedy.csv"
+]
+
+dfs = [pd.read_csv(os.path.join(current_directory, csv_directory, file)) for file in file_paths]
+
+for df in dfs:
+    df['Reduced'] = df['Reduced'].astype(int) * 23
+
+
+for df in dfs:
+    del df['Algorithm']
+    del df['Name']
+
+means = {}
+for df, file_path in zip(dfs, file_paths):
+    file_name = os.path.basename(file_path).split('.')[0]
+    means[file_name] = df.mean()
+
+modified_means = {}
+for key, value in means.items():
+    new_key = key.replace('files_', '').replace('_', ' ')
+    modified_means[new_key] = value
+
+means_df = pd.DataFrame(modified_means)
+
+
+summary_stats = means_df.describe()
+
+plt.figure(figsize=(10, 6))
+means_df.plot(kind='bar', rot=45, ax=plt.gca())
+plt.title('Comparison of Algorithms')
+plt.xlabel('Criterion')
+plt.ylabel('Value')
+plt.legend(title='Algorithm')
+plt.tight_layout()
+plt.show()
+
+
