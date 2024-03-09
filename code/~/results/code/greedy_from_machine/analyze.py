d72171353324af1aab35a50a42340369d9096b6e
or <or.fuchs12@gmail.com>
2024-03-09 13:46:54 +0200

    run after table fix with 3 csv

--- code/~/results/code/greedy_from_machine/analyze.py
+++ code/~/results/code/greedy_from_machine/analyze.py
@@ -21,8 +21,7 @@ file_paths = [
 dfs = [pd.read_csv(os.path.join(current_directory, csv_directory, file)) for file in file_paths]
 print(len(dfs[0]))
 for df in dfs:
-    df['Reduced'] = df['Reduced'].astype(int) * len(dfs[0])
-
+    df['Reduced'] = df['Reduced'].astype(int) * len(df)
 
 for df in dfs:
     del df['Algorithm']
@@ -39,11 +38,51 @@ for key, value in means.items():
     modified_means[new_key] = value
 
 means_df = pd.DataFrame(modified_means)
-
-
 summary_stats = means_df.describe()
 means_df.to_csv('code/~/results/code/greedy_from_machine/means.csv')
 print('means csv file created! ')
+
+reduced_means = {}
+for key, df in zip(means.keys(), dfs):
+    reduced_df = df[df['Reduced'].astype(bool)] 
+    reduced_means[key] = reduced_df.mean()
+
+modified_reduced_means = {}
+for key, value in reduced_means.items():
+    new_key = key.replace('files_', '').replace('_', ' ')
+    modified_reduced_means[new_key] = value
+
+for key in modified_means.keys():
+    modified_reduced_means[key]['Reduced'] = modified_means[key]['Reduced']
+reduced_means_df = pd.DataFrame(modified_reduced_means)
+
+reduced_means_df.to_csv('code/~/results/code/greedy_from_machine/reduced_means.csv')
+print('Reduced Means CSV file created!')
+
+
+reduced_rows = []
+for df in dfs:
+    reduced_rows.append(set(df[df['Reduced'].astype(bool)].index))
+
+intersection_rows = list(set.intersection(*reduced_rows))
+
+intersection_means = {}
+for df, file_path in zip(dfs, file_paths):
+    reduced_df = df.loc[intersection_rows]  
+    intersection_means[file_path] = reduced_df.mean()
+
+modified_intersection_means = {}
+for key, value in intersection_means.items():
+    file_name = os.path.basename(key).split('.')[0]
+    new_key = file_name.replace('files_', '').replace('_', ' ')
+    modified_intersection_means[new_key] = value
+
+for key in modified_means.keys():
+    modified_intersection_means[key]['Reduced'] = len(intersection_rows)
+intersection_means_df = pd.DataFrame(modified_intersection_means)
+
+intersection_means_df.to_csv('code/~/results/code/greedy_from_machine/intersection_means.csv')
+print('Intersection Means CSV file created!')
 
 plt.figure(figsize=(10, 6))
 means_df.plot(kind='bar', rot=45, ax=plt.gca())
@@ -53,4 +92,3 @@ plt.ylabel('Value')
 plt.legend(title='Algorithm')
 plt.tight_layout()
 plt.show()
-
