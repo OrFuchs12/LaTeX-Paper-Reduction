4f49e55db50d0c645b4fe2f1d654bec20b48dc61
or <or.fuchs12@gmail.com>
2024-04-15 20:29:47 +0300

    analyze results

--- code/~/results/code/greedy_from_machine/analyze.py
+++ code/~/results/code/greedy_from_machine/analyze.py
@@ -11,14 +11,14 @@ csv_directory = "code/~/results/code/greedy_from_machine"
 common_names = None
 
 file_paths = [
-    "files_results_simple_greedy.csv",
-    "files_results_heuristic_greedy.csv",
-    "files_results_non_stop_heuristic_greedy.csv",
-    "files_results_model_greedy.csv",
-    "files_non_stop_results_classification_greedy.csv",
-    "files_results_regreession_model_greedy.csv",
-    "files_results_non_stop_regreession_model_greedy.csv",
-    "files_results_classification_regreession_model_greedy.csv",
+    "results_simple_greedy.csv",
+    "results_heuristic_greedy.csv",
+    "results_non_stop_heuristic_greedy.csv",
+    "results_model_greedy.csv",
+    "non_stop_results_classification_greedy.csv",
+    "results_regreession_model_greedy.csv",
+    "results_non_stop_regreession_model_greedy.csv",
+    "results_classification_regreession_model_greedy.csv",
 ]
 
 dfs = [pd.read_csv(os.path.join(current_directory, csv_directory, file)) for file in file_paths]
